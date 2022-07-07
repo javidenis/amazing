0306e80de7c801dd2482942490517973b8aa3c7d
Jorge <jjavierdenis@gmail.com>
2022-07-07 15:44:36 -0500

    store for answer and questions should be done

---
+++ react-app/src/store/answers.js
@@ -0,0 +1,132 @@
+const GET_ANSWERS = '/answers/get'
+const ADD_ANSWERS = '/answers/add'
+const DELETE_ANSWERS = '/answers/delete'
+
+const actionAddAnswer = answer => {
+    return {
+        type: ADD_ANSWERS,
+        answer
+    }
+}
+
+const actionAllAnswers = answers => {
+    return {
+        type: GET_ANSWERS,
+        answers
+    }
+}
+
+const actionDeleteAnswer = answerId => {
+    return {
+        type: DELETE_ANSWERS,
+        answerId
+    }
+}
+
+export const deleteAnswerThunk = answerId => async dispatch => {
+    const response = await fetch(`/api/answer/${answerId}`,
+        {
+            method: "DELETE",
+        }
+    )
+    if (response.ok) {
+        dispatch(actionDeleteQuestion(answerId))
+    }
+}
+
+export const editAnswerThunk = answer => async dispatch => {
+
+    const {
+        content,
+        user_id,
+        question_id,
+        answerId
+    } = answer
+
+    const formData = new FormData()
+    formData.append('content', content)
+    formData.append('user_id', user_id)
+    formData.append('question_id', question_id)
+    formData.append('answerId', answerId)
+
+    const response = await fetch('/api/answers',
+        {
+            method: "PUT",
+            body: formData
+        }
+    )
+    const data = await response.json()
+    if (response.ok) {
+        dispatch(actionAddAnswer(data))
+        return null
+    } else if (response.status < 500) {
+        if (data.errors) {
+            return data.errors;
+        }
+    } else {
+        return ['An error occurred. Please try again.']
+    }
+}
+
+export const getAnswersThunk = () => async dispatch => {
+    const response = await fetch('/api/answers')
+    const data = await response.json()
+    if (response.ok) {
+        dispatch(actionAllAnswers(data))
+    }
+}
+
+export const addAnswerThunk = newAnswer => async dispatch => {
+    const {
+        content,
+        user_id,
+        question_id
+    } = newAnswer
+
+    const formData = new FormData()
+    formData.append('content', content)
+    formData.append('user_id', user_id)
+    formData.append('question_id', question_id)
+
+
+    const response = await fetch('/api/answers', {
+        method: "POST",
+        body: formData
+    }
+    )
+
+    const data = await response.json()
+    if (response.ok) {
+        dispatch(actionAddAnswer(data))
+        return null
+    } else if (response.status < 500) {
+        if (data.errors) {
+            return data.errors;
+        }
+    } else {
+        return ['An error occurred. Please try again.']
+    }
+}
+
+const answerReducer = (state = {}, action) => {
+    switch (action.type) {
+        case ADD_ANSWERS:
+            let newAddState = {}
+            newAddState = { ...state, [action.answer.id]: action.answer }
+            return newAddState
+        case GET_ANSWERS:
+            let newState = { ...state };
+            action.answers.answers.forEach((answer) => {
+                newState[answer.id] = answer;
+            });
+            return newState;
+        case DELETE_ANSWERS:
+            let newDeleteState = { ...state };
+            delete newDeleteState[action.answerId]
+            return newDeleteState
+        default:
+            return state
+    }
+}
+
+export default questionReducer
--- react-app/src/store/questions.js
+++ react-app/src/store/questions.js
@@ -40,12 +40,14 @@ export const editQuestionThunk = question => async dispatch => {
         title,
         content,
         user_id,
+        questionId
     } = question
 
     const formData = new FormData()
     formData.append('title', title)
     formData.append('content', content)
     formData.append('user_id', user_id)
+    formData.append('questionId', questionId)
 
     const response = await fetch('/api/questions',
         {
@@ -73,3 +75,58 @@ export const getQuestionsThunk = () => async dispatch => {
         dispatch(actionAllQuestions(data))
     }
 }
+
+export const addQuestionThunk = newQuestion => async dispatch => {
+    const {
+        title,
+        content,
+        user_id,
+    } = newQuestion
+
+    const formData = new FormData()
+    formData.append('title', title)
+    formData.append('content', content)
+    formData.append('user_id', user_id)
+
+
+    const response = await fetch('/api/questions', {
+        method: "POST",
+        body: formData
+    }
+    )
+
+    const data = await response.json()
+    if (response.ok) {
+        dispatch(actionAddQuestion(data))
+        return null
+    } else if (response.status < 500) {
+        if (data.errors) {
+            return data.errors;
+        }
+    } else {
+        return ['An error occurred. Please try again.']
+    }
+}
+
+const questionReducer = (state = {}, action) => {
+    switch (action.type) {
+        case ADD_QUESTION:
+            let newAddState = {}
+            newAddState = { ...state, [action.question.id]: action.question }
+            return newAddState
+        case GET_QUESTIONS:
+            let newState = { ...state };
+            action.questions.questions.forEach((question) => {
+                newState[question.id] = question;
+            });
+            return newState;
+        case DELETE_QUESTION:
+            let newDeleteState = { ...state };
+            delete newDeleteState[action.questionId]
+            return newDeleteState
+        default:
+            return state
+    }
+}
+
+export default questionReducer
