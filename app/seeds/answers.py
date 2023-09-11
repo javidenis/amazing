f2212353c38775c277e41ba15a932ca866193421
Jorge <jjavierdenis@gmail.com>
2023-09-11 13:22:29 -0500

    fixing render deploy issue x3

--- app/seeds/answers.py
+++ app/seeds/answers.py
@@ -4,9 +4,9 @@ def seed_answers():
   answer3 = Answer(content="That's something a billionaire would say", question_id= 2, user_id= 2)
   answer4 = Answer(content="If I were you, I wouldn't visit the Amazon River", question_id= 3, user_id= 2)
   answer5 = Answer(content="Dude just buy another toilet...", question_id= 4, user_id= 1)
-  answer7 = Answer(content="People just care about getting a video viral and get 'famous' for a second before helping", question_id= 6, user_id= 2)
-  answer8 = Answer(content="That's probably on the bible", question_id= 7, user_id= 2)
-  answer9 = Answer(content="Human as Homo sapiens or all the way back to Homo naledi? And I know a question as an answer for another question but you need to specify more", question_id= 7, user_id= 1)
+  answer7 = Answer(content="People just care about getting a video viral and get 'famous' for a second before helping", question_id= 3, user_id= 2)
+  answer8 = Answer(content="That's probably on the bible", question_id= 5, user_id= 2)
+  answer9 = Answer(content="Human as Homo sapiens or all the way back to Homo naledi? And I know a question as an answer for another question but you need to specify more", question_id= 5, user_id= 1)
  
   db.session.add(answer3)
   db.session.add(answer4)
